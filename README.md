5852b8314e115b49fa71a77035160edc40da895c
Cesar Kastli <cesarkastli@gmail.com>
2015-10-04 03:38:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+jQuery Options
+
+```javascript
+$('#my-select-input').options([ 'text/plain', 'application/json', 'image/png' ], 'Select the content type...');
+```
+
+```javascript
+$('#my-select-input').options([
+  { name: 'Español (Argentina)', value: 'es_AR' },
+  { name: 'English (United States)', value: 'en_US' },
+  { name: 'Português (Brasil)', value: 'pt_BR' }
+]);
+```
---
+++ jquery.options.js
@@ -0,0 +1,40 @@
+;(function(root, undefined) {
+
+  var pluginName = 'options';
+
+  var $ = root.jQuery;
+
+  if ('undefined' === typeof $) {
+    if ('undefined' === typeof require) {
+      throw new Error('The plugin "' + pluginName + '" requires jQuery');
+    }
+    $ = require('jquery');
+  }
+
+  $.fn[pluginName] = function(options, blankOption) {
+    return this.each(function() {
+      var $elem = $(this).empty();
+      if (blankOption) {
+        $elem.append(new Option(blankOption, ''));
+      }
+      $.each(options, function(index, item) {
+        var value, name;
+        switch ($.type(item)) {
+          case 'array':
+            value = item[0];
+            name = item[1];
+            break;
+          case 'object':
+            value = item.value || item.val;
+            name = item.name || item.key;
+            break;
+          default:
+            value = item;
+            name = item;
+        }
+        $elem.append(new Option(name, value));
+      });
+    });
+  };
+
+}(this));
